5833b9a033378d23ec5c3fea6b0295b6f4e6112f
Ynifa <yasine.nifa@gmail.com>
2022-03-09 21:50:25 +0100

    Add icons in the sidebar
    Signed-off-by: Ynifa <yasine.nifa@gmail.com>

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import "./Sidebar.scss"
-
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import SettingsIcon from '@mui/icons-material/Settings';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import LogoutIcon from '@mui/icons-material/Logout';
 function Sidebar() {
   return (
     <div className='sidebar'>
@@ -11,21 +22,53 @@ function Sidebar() {
         <div className='center'>
             <ul>
                 <li>
+                    <DashboardIcon />
                     <span>Dashboard</span>
-                
+                </li>
+                <li>
+                    <PersonOutlinedIcon/>
+                    <span>Users</span>
                 </li> 
                 <li>
-                    <span>Dashboard</span>
-                
+                    <AddBusinessIcon/>
+                    <span>Products</span>
                 </li> 
                 <li>
-                    <span>Dashboard</span>
-                
-                </li> 
+                    <CreditCardIcon/>
+                    <span>Orders</span>
+                </li>
                 <li>
-                    <span>Dashboard</span>
-                
-                </li>    
+                    <LocalShippingIcon/>
+                    <span>Delivery</span>
+                </li>
+                <li>
+                    <QueryStatsIcon/>
+                    <span>Stats</span>
+                </li>
+                <li>
+                    <NotificationsActiveIcon/>
+                    <span>Notifications</span>
+                </li>
+                <li>
+                    <LocalHospitalIcon/>
+                    <span>System Health</span>
+                </li>
+                <li>
+                    <PsychologyIcon/>
+                    <span>Logs</span>
+                </li>
+                <li>
+                    <SettingsIcon/>
+                    <span>Settings</span>
+                </li>
+                <li>
+                    <AccountBoxIcon/>
+                    <span>Profile</span>
+                </li>
+                <li>
+                    <LogoutIcon/>
+                    <span>Logout</span>
+                </li>
             </ul>    
         </div>
         <div className='bottom'>color options</div>  
