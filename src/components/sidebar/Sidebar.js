684e8a8ff06756bbbebda8b7714776da9cd19797
Ynifa <yasine.nifa@gmail.com>
2022-03-09 22:04:52 +0100

    Style the center part of the sidebar
    Signed-off-by: Ynifa <yasine.nifa@gmail.com>

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -21,52 +21,57 @@ function Sidebar() {
         <hr/>{/*add an horizontal line*/}
         <div className='center'>
             <ul>
+                <p className="title">MAIN</p>
                 <li>
-                    <DashboardIcon />
+                    <DashboardIcon className="icon"/>
                     <span>Dashboard</span>
                 </li>
+                <p className="title">LISTS</p>
                 <li>
-                    <PersonOutlinedIcon/>
+                    <PersonOutlinedIcon className="icon"/>
                     <span>Users</span>
                 </li> 
                 <li>
-                    <AddBusinessIcon/>
+                    <AddBusinessIcon className="icon"/>
                     <span>Products</span>
                 </li> 
                 <li>
-                    <CreditCardIcon/>
+                    <CreditCardIcon className="icon"/>
                     <span>Orders</span>
                 </li>
                 <li>
-                    <LocalShippingIcon/>
+                    <LocalShippingIcon className="icon"/>
                     <span>Delivery</span>
                 </li>
+                <p className="title">USEFUL</p>
                 <li>
-                    <QueryStatsIcon/>
+                    <QueryStatsIcon className="icon"/>
                     <span>Stats</span>
                 </li>
                 <li>
-                    <NotificationsActiveIcon/>
+                    <NotificationsActiveIcon className="icon"/>
                     <span>Notifications</span>
                 </li>
+                <p className="title">SERVICE</p>
                 <li>
-                    <LocalHospitalIcon/>
+                    <LocalHospitalIcon className="icon"/>
                     <span>System Health</span>
                 </li>
                 <li>
-                    <PsychologyIcon/>
+                    <PsychologyIcon className="icon"/>
                     <span>Logs</span>
                 </li>
                 <li>
-                    <SettingsIcon/>
+                    <SettingsIcon className="icon"/>
                     <span>Settings</span>
                 </li>
+                <p className="title">USER</p>
                 <li>
-                    <AccountBoxIcon/>
+                    <AccountBoxIcon className="icon"/>
                     <span>Profile</span>
                 </li>
                 <li>
-                    <LogoutIcon/>
+                    <LogoutIcon className="icon"/>
                     <span>Logout</span>
                 </li>
             </ul>    
