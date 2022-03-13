5bd1f66e0fe6cad6d729d04eef3245ef51169a20
Ynifa <yasine.nifa@gmail.com>
2022-03-13 18:07:26 +0100

    Add new items in the header
    Signed-off-by: Ynifa <yasine.nifa@gmail.com>

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import "./Navbar.scss"
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LanguageOutlinedIcon from "@mui/icons-material/LanguageOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
+import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
 
 function Navbar() {
   return (
@@ -9,6 +15,28 @@ function Navbar() {
         <div className='search'>
           <input placeholder='Search...' type="text"></input>
           <SearchOutlinedIcon/>
+        </div>
+        <div className='items'>
+          <div className='item'>
+            <LanguageOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <DarkModeOutlinedIcon
+              className="icon"
+            />
+          </div>
+          <div className="item">
+            <FullscreenExitOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <NotificationsNoneOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <ChatBubbleOutlineOutlinedIcon className="icon" />
+          </div>
+          <div className="item">
+            <ListOutlinedIcon className="icon" />
+          </div>
         </div>
 
       </div>
